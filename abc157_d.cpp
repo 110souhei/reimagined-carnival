c9d79f084b12f124a8b5a4e1351695adc7e94846
110souhei <67193993+110souhei@users.noreply.github.com>
2022-06-12 16:04:39 +0900

    Add files via upload

---
+++ abc157_d.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0; i< (n); i++)
+using namespace std;
+typedef long long ll;
+typedef pair<int , int> P;
+const ll inf = (1ll<<60);
+//const int mod = 998244353;
+const int mod = 1000000007;
